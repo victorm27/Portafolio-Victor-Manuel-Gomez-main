5df4ec8cac62e44cbbec9bf106907e4091630857
Victor Manuel Gomez <victormanuel272791@egmail.com>
2024-04-24 19:06:38 -0500

    mobile

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+const navMenu = document.querySelector('.nav-menu');
+const navToggle = document.querySelector('.nav-toggle');
+
+document.addEventListener('DOMContentLoaded', function() {
+    let typed = new Typed(".text", {
+        strings: ["Desarrollador Backend", "Desarrollador Frontend", "Desarrollador Fullstack"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    }
+    );
+});
+
+
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
